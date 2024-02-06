3e6490f1083c6c90e8fcdc1e93b705687dd67265
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-05 16:58:16 -0800

    Creating the repository for the capstone project.

---
+++ Capstone Project/src/Footer.jsx
@@ -0,0 +1,17 @@
+
+function Footer(){
+
+    return(
+        <div>
+            <p>Test</p>
+            <ul>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Documentation</a></li>
+                <li><a href="">Resources</a></li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default Footer
