f7cfc5cb3bfb801d5bbaf3815af6e7cd3c595716
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-05 16:28:43 -0800

    Creating the repository for the capstone project.

---
+++ Capstone Project/src/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>About Us</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            <h1>Demo Website</h1>
+        </header>
+    )
+}
+
+export default Header
