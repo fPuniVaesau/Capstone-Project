712b15ac2fd2473368aafa4a3c1b0b4b4d656cc0
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-06 17:07:31 -0800

    working on basics for the react framework.

--- Capstone Project/src/App.jsx
+++ Capstone Project/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Components/Header.jsx"
 import Footer from "./Components/Footer.jsx"
+import Main from "./Components/Main.jsx"
 
 function App() {
 
   return(
     <>
     <Header></Header>
+    <Main></Main>
     <Footer></Footer>
     </>
   )
---
+++ Capstone Project/src/Components/Main.jsx
@@ -0,0 +1,14 @@
+
+function Main (){
+    return(
+        <>
+            <section>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro saepe odit fugit ipsam debitis a consequatur. Totam nihil dolore quas fugiat consectetur illo obcaecati, est, earum fugit porro necessitatibus. Eligendi.</section>
+            <blockquote>
+                <q>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, deleniti!</q>
+            </blockquote>
+            <section>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quod ipsam atque praesentium at hic sed commodi obcaecati eius possimus, quas eveniet consequuntur vero, quis mollitia ratione blanditiis. Esse, velit!</section>
+        </>
+    );
+}
+
+export default Main;
