f7885fe48df6732dc8f4ec5398d1a76855b0f1b3
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-08 18:22:08 -0800

    working on some react notes.

--- Capstone Project/src/Components/Main.jsx
+++ Capstone Project/src/Components/Main.jsx
@@ -1,14 +1,26 @@
 
-function Main (){
+export default function Main (){
+    const students = [
+        {
+            name: 'Filo Puni Vaesau',
+            age: 31,
+            location: 'San Francisco, Ca',
+            role: ['Full Stack Developer', 'Digital Craftsman']
+        },
+        {
+            name: 'Morgan Simone Vaesau',
+            age: 26,
+            location: 'Castro Valley, Ca',
+            role: ['Social Worker']
+        }
+    ]
+
     return(
-        <>
-            <section>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro saepe odit fugit ipsam debitis a consequatur. Totam nihil dolore quas fugiat consectetur illo obcaecati, est, earum fugit porro necessitatibus. Eligendi.</section>
-            <blockquote>
-                <q>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, deleniti!</q>
-            </blockquote>
-            <section>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quod ipsam atque praesentium at hic sed commodi obcaecati eius possimus, quas eveniet consequuntur vero, quis mollitia ratione blanditiis. Esse, velit!</section>
-        </>
+
+        <div>
+            {students.map(student=>{return <li>{student.name} | {student.role[0]} | {student.location}
+            </li>})}
+        </div>
     );
 }
 
-export default Main;
