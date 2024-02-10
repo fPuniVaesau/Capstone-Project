224ebbafa8b3bcfb1b2d66d1f586926aca8b8f4c
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-09 17:29:37 -0800

    learning how to create components into react.

--- Capstone Project/src/Components/ProfileCard.jsx
+++ Capstone Project/src/Components/ProfileCard.jsx
@@ -1,8 +1,19 @@
 export default function ProfileCard(){
 
+    const profileInfo = [
+        {
+            name: "Filo Puni Vaesau",
+            role: ['Full Stack Developer', 'Digital Craftsman'],
+            size: '250px'
+        }
+    
+    ]
+
     return(
         <div>
-            
+            <img src="" alt="" style={{width: profileInfo.size, height: profileInfo.size}}/>
+            <h1>{profileInfo.name}</h1>
+            <h3>{profileInfo.role}</h3>
         </div>
     )
 }
