fdf93676229f3d773653a50cdf6d81e14a7cc224
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-09 21:48:13 -0800

    learning how to create components into react.

--- Capstone Project/src/App.jsx
+++ Capstone Project/src/App.jsx
@@ -1,6 +1,7 @@
 import Button from "./Components/Button.jsx"
 import Navigation from "./Components/Navigation.jsx"
 import Profiles from "./Components/Profiles.jsx"
+import ProfileCard from "./Components/ProfileCard.jsx"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Navigation elOne="About" elTwo="Projects" elThree='Contact' ></Navigation>
       <Button content="Subscribe"></Button>
       <Profiles></Profiles>
+      <ProfileCard ></ProfileCard>
     </div>
   )
 }
--- Capstone Project/src/Components/ProfileCard.jsx
+++ Capstone Project/src/Components/ProfileCard.jsx
@@ -1,4 +1,4 @@
-export default function ProfileCard(){
+export default function ProfileCard({}){
 
     const profileInfo = [
         {
