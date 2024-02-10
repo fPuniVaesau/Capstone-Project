b74b9b8980a6ccb3dd1af00005967ddbcf3c8182
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-10 14:29:43 -0800

    learning how to create components into react.

--- Capstone Project/src/Components/ProfileCard.jsx
+++ Capstone Project/src/Components/ProfileCard.jsx
@@ -11,7 +11,7 @@ export default function ProfileCard({}){
 
     return(
         <div>
-            <img src="" alt="" style={{width: profileInfo.size, height: profileInfo.size}}/>
+            <img src="https://media.licdn.com/dms/image/D5603AQGM_vst3jwX6Q/profile-displayphoto-shrink_400_400/0/1699300130843?e=1712793600&v=beta&t=S6wYNYMcTlIMZ2eLMYY0x0G7GRHNGa-6BYVa6u0dN00" alt="" style={{width: profileInfo.size, height: profileInfo.size}}/>
             <h1>{profileInfo.name}</h1>
             <h3>{profileInfo.role}</h3>
         </div>
