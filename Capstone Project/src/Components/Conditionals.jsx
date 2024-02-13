d8f4d2d9ddaa0dd0471e20329c310797c269ea15
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-13 10:59:17 -0800

    learning how to create components into react.

--- Capstone Project/src/Components/Conditionals.jsx
+++ Capstone Project/src/Components/Conditionals.jsx
@@ -1,9 +1,7 @@
-export default function Conditionals(){
+export default function Conditionals() {
+  let test = true;
 
-    let test = true;
-
-    return(
-        <div>{test === true ? <h1>Welcome Filo</h1> : <p>Please log in</p>}</div>
-    )
-    
+  return (
+    <div>{test === true ? <h1>Welcome Filo</h1> : <p>Please log in</p>}</div>
+  );
 }
