337a32481b4d0fd65c8e03af0ee31e1a169643e7
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-16 20:04:35 -0800

    learning how to create components into react.

--- Capstone Project/src/App.jsx
+++ Capstone Project/src/App.jsx
@@ -3,7 +3,7 @@ import Navigation from "./Components/Navigation.jsx"
 import Profiles from "./Components/Profiles.jsx"
 import ProfileCard from "./Components/ProfileCard.jsx"
 import Conditionals from "./Components/Conditionals.jsx"
-import Sandbox from "./Components/sandbox.jsx"
+
 
 function App() {
 
@@ -14,7 +14,7 @@ function App() {
       {/* <Profiles></Profiles> */}
       <ProfileCard ></ProfileCard>
       <Conditionals />
-      <Sandbox />
+
     </div>
   )
 }
