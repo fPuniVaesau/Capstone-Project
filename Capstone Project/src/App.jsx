08096aac893131934756c2a508cf16f50b2f8bca
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-16 20:04:52 -0800

    learning how to create components into react.

--- Capstone Project/src/App.jsx
+++ Capstone Project/src/App.jsx
@@ -1,6 +1,5 @@
 import Button from "./Components/Button.jsx"
 import Navigation from "./Components/Navigation.jsx"
-import Profiles from "./Components/Profiles.jsx"
 import ProfileCard from "./Components/ProfileCard.jsx"
 import Conditionals from "./Components/Conditionals.jsx"
 
@@ -11,7 +10,6 @@ function App() {
     <div>
       <Navigation elOne="About" elTwo="Projects" elThree='Contact' ></Navigation>
       <Button content="Subscribe"></Button>
-      {/* <Profiles></Profiles> */}
       <ProfileCard ></ProfileCard>
       <Conditionals />
 
