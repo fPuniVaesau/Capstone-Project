1d3cdfd115e82a8088381c47a53d7e43e9dd7986
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-17 18:05:10 -0800

    working on the layout for the capstone project.

--- Capstone Project/src/App.jsx
+++ Capstone Project/src/App.jsx
@@ -1,5 +1,6 @@
 import Navigation from "./Components/Navigation"
 import ProfileCard from "./Components/ProfileCard"
+import Button from "./Components/Button"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navigation elOne="About" elTwo='Projects' elThree="Contact Me"/>
       <ProfileCard />
+
     </>
   )
 }
