c518ccfc86e3f915b3911f087c0fca69c8fa6574
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-19 10:13:57 -0800

    working with useState and the onChange prop used for form handling.

--- Capstone Project/src/Components/Form2.jsx
+++ Capstone Project/src/Components/Form2.jsx
@@ -5,13 +5,18 @@ export default function Form2(){
     let [value, setValue] = useState({FirstName:'', LastName:''});
 
     const handleFirstName = (e) => {
-        setValue({FirstName:e.target.value})
+        setValue({...value, FirstName: e.target.value})
+    }
+
+    const handleLastName = (e) => {
+        setValue({...value, LastName: e.target.value})
     }
 
     return(
         <>
             <form>
                 <input type="text" onChange={(e)=>{handleFirstName(e)}}/>
+                <input type="text" onChange={(e)=>{handleLastName(e)}}/>
             </form>
         </>
     )
