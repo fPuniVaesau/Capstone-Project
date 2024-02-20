fab52c07595947184f34782cec7edce79a1b80cf
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-20 13:31:14 -0800

    working on a login component.

--- Capstone Project/src/Components/LogInComponents/Login.jsx
+++ Capstone Project/src/Components/LogInComponents/Login.jsx
@@ -1,27 +1,36 @@
-import { useState } from "react"
+import { useState } from 'react';
 
+export default function Login() {
+  let [credentials, setCredentials] = useState({ UserName: '', PassWord: '' });
 
-export default function Login(){
-    let [credentials, setCredentials] = useState({UserName:"", PassWord:""});
+  let setUserName = (e) => {
+    setCredentials({ ...credentials, UserName: e.target.value });
+  };
 
-    let setUserName = (e) => {
-        setCredentials({...credentials, UserName:e.target.value})
-    }
+  let setPassword = () => {
+    setCredentials({ ...credentials, PassWord: e.target.value });
+  };
 
-    let setPassword = () => {
-        setCredentials({...credentials, PassWord:e.target.value})
-    }
-
-    return(
-        <>
-            <h3>Login</h3>
-            <form>
-                <label htmlFor="">Username</label>
-                <input type="text" onChange={(e)=>{setUserName(e)}}/>
-                <br></br>
-                <label htmlFor="">Password</label>
-                <input type="text" onChange={(e)=>{setPassword(e)}}/>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <h3>Login</h3>
+      <form>
+        <label htmlFor=''>Username</label>
+        <input
+          type='text'
+          onChange={(e) => {
+            setUserName(e);
+          }}
+        />
+        <br></br>
+        <label htmlFor=''>Password</label>
+        <input
+          type='password'
+          onChange={(e) => {
+            setPassword(e);
+          }}
+        />
+      </form>
+    </>
+  );
 }
