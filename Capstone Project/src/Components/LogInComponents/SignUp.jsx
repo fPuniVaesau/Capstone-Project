1831586e2b3770ad8ef3226d19ed1a8cd3ea70ed
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-20 13:35:12 -0800

    working on a signup component.

--- Capstone Project/src/Components/LogInComponents/SignUp.jsx
+++ Capstone Project/src/Components/LogInComponents/SignUp.jsx
@@ -3,7 +3,10 @@ export default function SignUp(){
     return(
         <>
             <h3>Sign Up</h3>
-            
+            <form action="">
+                <label htmlFor="">First Name</label>
+                <input type="text" />
+            </form>
         </>
     )
 }
