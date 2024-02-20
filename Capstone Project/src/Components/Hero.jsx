96fd50a7bbbe85b45e415326c5a092527589aa68
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-20 12:20:42 -0800

    Working on a component for the structure of the page.

--- Capstone Project/src/Components/Hero.jsx
+++ Capstone Project/src/Components/Hero.jsx
@@ -4,6 +4,7 @@ export default function Hero({title, subtitle}){
         <>
             <h1>{title}</h1>
             <h2>{subtitle}</h2>
+            <h3>This is a demo</h3>
         </>
     )
 }
