9acee24a18984b3f3f7de589b89ad8366ed81970
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-20 13:20:12 -0800

    Working on a component for the structure of the page.

---
+++ Capstone Project/src/Components/LogInComponents/Login.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+
+export default function Login(){
+
+
+    return(
+        <>
+            <h3>Login</h3>
+            <form>
+                <label htmlFor="">Username</label>
+                <input type="text" />
+            </form>
+        </>
+    )
+}
