ff33e0367024cc09ff5ff24761ff5dfce75cd9ee
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-20 13:43:35 -0800

    working on a signup component.

--- Capstone Project/src/Components/LogInComponents/SignUp.jsx
+++ Capstone Project/src/Components/LogInComponents/SignUp.jsx
@@ -1,35 +1,33 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-export default function SignUp(){
-    let[credentials, setCredentials] = useState({
-        firstName: '',
-        lastName: '',
-        age: 0,
-        userName: '',
-        password: '',
-    })
+export default function SignUp() {
+  let [credentials, setCredentials] = useState({
+    firstName: '',
+    lastName: '',
+    age: 0,
+    userName: '',
+    password: '',
+  });
 
-    return(
-        <>
-            <h3>Sign Up</h3>
-            <form action="">
-                <label htmlFor="">First Name</label>
-                <input type="text" />
-                <br />
-                <label htmlFor="">Last Name</label>
-                <input type="password" />
-                <br />
-                <label htmlFor="">Age</label>
-                <input type="number" />
-                <br />
-                <label htmlFor="">UserName</label>
-                <input type="text" />
-                <br />
-                <label htmlFor="">Password</label>
-                <input type="text" />
-                <br />
-
-            </form>
-        </>
-    )
+  return (
+    <>
+      <h3>Sign Up</h3>
+      <form action=''>
+        <label htmlFor=''>First Name</label>
+        <input type='text' />
+        <br />
+        <label htmlFor=''>Last Name</label>
+        <input type='password' />
+        <br />
+        <label htmlFor=''>Age</label>
+        <input type='number' />
+        <br />
+        <label htmlFor=''>UserName</label>
+        <input type='text' />
+        <br />
+        <label htmlFor=''>Password</label>
+        <input type='text' />
+      </form>
+    </>
+  );
 }
