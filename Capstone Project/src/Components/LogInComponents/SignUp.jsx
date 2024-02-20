c868a117058a047e46a3069ff2b4fda5e1237a94
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-20 13:37:51 -0800

    working on a signup component.

--- Capstone Project/src/Components/LogInComponents/SignUp.jsx
+++ Capstone Project/src/Components/LogInComponents/SignUp.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react"
 
 export default function SignUp(){
+    let[credentials, setCredentials] = useState({
+        firstName: '',
+        lastName: '',
+        
+        userName: '',
+        password: '',
+    })
+
     return(
         <>
             <h3>Sign Up</h3>
