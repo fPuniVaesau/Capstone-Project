3f1d6d538d017dab109cb52b383f322d682bc0ae
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-20 17:06:21 -0800

    working on a signup component.

--- Capstone Project/src/Components/LogInComponents/SignUp.jsx
+++ Capstone Project/src/Components/LogInComponents/SignUp.jsx
@@ -39,7 +39,7 @@ export default function SignUp() {
         <input type='text' onChange={(e) => setFirstName(e)} />
         <br />
         <label htmlFor=''>Last Name</label>
-        <input type='password' onChange={(e) => setLastName(e)}/>
+        <input type='text' onChange={(e) => setLastName(e)}/>
         <br />
         <label htmlFor=''>Age</label>
         <input type='number' onChange={(e)=>setAge(e)}/>
