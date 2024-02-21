b8216a64174b957c4db87cc842fbcf95648fae86
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-21 13:14:02 -0800

    working on a signup component.

--- Capstone Project/src/Components/LogInComponents/LoginSignUp.jsx
+++ Capstone Project/src/Components/LogInComponents/LoginSignUp.jsx
@@ -2,10 +2,8 @@ import React, { useState } from 'react';
 import Login from './Login';
 import SignUp from './SignUp';
 
-export default function LoginSignUp() {
-  return (
-    <>
-      <h3>Test</h3>
-    </>
-  );
+export default function LoginSignUp({prop}) {
+  let [userStatus, setUserStatus] = useState(False)
+  
+  userStatus === true ? <Login /> : <SignUp />
 }
