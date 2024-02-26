9fef933780de44bae0fa15fe3cc8a511b8516ab4
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-26 12:32:25 -0800

    working on read me profile for github account.

--- Capstone Project/src/Components/LogInComponents/Time.jsx
+++ Capstone Project/src/Components/LogInComponents/Time.jsx
@@ -2,10 +2,13 @@
 export default function Time(){
     let date = new Date();
     let showTime = date.getHours()%12 + " | " + date.getMinutes() + " | " + date.getSeconds();
+    let showDay = date.getDay();
+    let dayOftheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
 
     return(
         <>
             <h3>testing the time</h3>
+            <p>{dayOftheWeek[showDay]}</p>
             <p>{showTime}</p>
         </>
     )
