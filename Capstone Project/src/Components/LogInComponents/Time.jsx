53211e95616849155ff4712b39d68bc841983945
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-26 10:29:23 -0800

    working on read me profile for github account.

--- Capstone Project/src/Components/LogInComponents/Time.jsx
+++ Capstone Project/src/Components/LogInComponents/Time.jsx
@@ -1,12 +1,12 @@
 
 export default function Time(){
     let date = new Date();
-    let showTime = date.getHours()%12
+    let showTime = date.getHours()%12 + " | " + date.getMinutes() + " | " + date.getSeconds();
 
     return(
         <>
             <h3>testing the time</h3>
-            <p>{}</p>
+            <p>{showTime}</p>
         </>
     )
 }
