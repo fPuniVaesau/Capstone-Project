cf6f05056788133c493fa19c6ead5a490e8d0d3b
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-27 10:47:46 -0800

    working on read me profile for github account.

--- Capstone Project/src/Components/LogInComponents/Time.jsx
+++ Capstone Project/src/Components/LogInComponents/Time.jsx
@@ -1,16 +1,28 @@
+export default function Time() {
+  let date = new Date();
+  let showTime =
+    (date.getHours() % 12) +
+    ' | ' +
+    date.getMinutes() +
+    ' | ' +
+    date.getSeconds();
+  let showDay = date.getDay();
+  let dayOftheWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
-export default function Time(){
-    let date = new Date();
-    let showTime = date.getHours()%12 + " | " + date.getMinutes() + " | " + date.getSeconds();
-    let showDay = date.getDay();
-    let dayOftheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
-
-    return(
-        <>
-            <h3>testing the time</h3>
-            <p>{dayOftheWeek[showDay]}</p>
-            <p>{showTime}</p>
-        </>
-    )
+  return (
+    <>
+      <h2>This is a time componenet</h2>
+      <h3>testing the time</h3>
+      <p>{dayOftheWeek[showDay]}</p>
+      <p>{showTime}</p>
+    </>
+  );
 }
-
