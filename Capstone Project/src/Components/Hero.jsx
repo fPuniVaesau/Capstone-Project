bf4d7a878b288ab8ff55c6f7ddba58253c1fd49a
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-28 13:41:35 -0800

    working on read me profile for github account.

--- Capstone Project/src/Components/Hero.jsx
+++ Capstone Project/src/Components/Hero.jsx
@@ -1,14 +1,18 @@
+export default function Hero({ title, subtitle }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+      <h3>Filo Puni Vaesau | Digital Craftsman / Full Stack Developer</h3>
 
-export default function Hero({title, subtitle}){
-    return(
-        <>
-            <h1>{title}</h1>
-            <h2>{subtitle}</h2>
-            <h3>Filo Puni Vaesau | Digital Craftsman / Full Stack Developer</h3>
-
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis tenetur laboriosam ratione, quas dolores debitis adipisci vitae aspernatur perspiciatis, id, deserunt et delectus tempore corrupti aperiam recusandae suscipit voluptates dolor.</p>
-            </div>
-        </>
-    )
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+          tenetur laboriosam ratione, quas dolores debitis adipisci vitae
+          aspernatur perspiciatis, id, deserunt et delectus tempore corrupti
+          aperiam recusandae suscipit voluptates dolor.
+        </p>
+      </div>
+    </div>
+  );
 }
