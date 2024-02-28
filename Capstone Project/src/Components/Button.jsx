e0ffdc54c99cfb1c38d3260f3bae6c335f839bd2
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-28 13:39:58 -0800

    working on read me profile for github account.

--- Capstone Project/src/Components/Button.jsx
+++ Capstone Project/src/Components/Button.jsx
@@ -1,13 +1,13 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 
 export default function Button() {
-   let [number, setNumber] = useState(0)
+  let [number, setNumber] = useState(0);
 
-    return(
-        <div>
-            <p>Choose a color!</p>
-            <button onClick={()=>setNumber(number)}>Set Number</button>
-            <p>Your favoiite # is: {number}</p>
-        </div>
-    );
+  return (
+    <div>
+      <p>Choose a color!</p>
+      <button onClick={() => setNumber(number)}>Set Number</button>
+      <p>Your favoiite # is: {number}</p>
+    </div>
+  );
 }
