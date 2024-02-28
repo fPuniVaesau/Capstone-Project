c3a3aaa2c32b694903312e9329887b4808f12286
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-02-28 13:41:04 -0800

    working on read me profile for github account.

--- Capstone Project/src/Components/Profiles.jsx
+++ Capstone Project/src/Components/Profiles.jsx
@@ -1,32 +1,35 @@
+export default function Profiles() {
+  const students = [
+    {
+      name: 'Filo Puni Vaesau',
+      age: 31,
+      location: 'San Francisco, Ca',
+      role: ['Full Stack Developer', 'Digital Craftsman'],
+    },
+    {
+      name: 'Morgan Simone Vaesau',
+      age: 26,
+      location: 'Castro Valley, Ca',
+      role: ['Social Worker'],
+    },
+    {
+      name: 'Milo Vaesau',
+      age: 10,
+      location: 'Dublin, Ca',
+      role: ['Full Time Student'],
+    },
+  ];
 
-export default function Profiles (){
-    const students = [
-        {
-            name: 'Filo Puni Vaesau',
-            age: 31,
-            location: 'San Francisco, Ca',
-            role: ['Full Stack Developer', 'Digital Craftsman']
-        },
-        {
-            name: 'Morgan Simone Vaesau',
-            age: 26,
-            location: 'Castro Valley, Ca',
-            role: ['Social Worker']
-        },
-        {
-            name: 'Milo Vaesau',
-            age: 10,
-            location: 'Dublin, Ca',
-            role: ['Full Time Student']
-        }
-    ]
-
-    return(
-        <div>
-            <p>This is a component creating a list using objects</p>
-            {students.map(student=>{return <li>{student.name} | {student.role[0]} | {student.location}
-            </li>})}
-        </div>
-    );
+  return (
+    <div>
+      <p>This is a component creating a list using objects</p>
+      {students.map((student) => {
+        return (
+          <li>
+            {student.name} | {student.role[0]} | {student.location}
+          </li>
+        );
+      })}
+    </div>
+  );
 }
-
