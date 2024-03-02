fe7af638c0cb5074b7c25c2c6db40b209b0cc260
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-02 12:21:59 -0800

    working on the base components for the capstone project.

--- Capstone Project/src/App.jsx
+++ Capstone Project/src/App.jsx
@@ -1,32 +1,10 @@
-import Navigation from './Components/Navigation';
-import ProfileCard from './Components/ProfileCard';
-import Button from './Components/Button';
-import UseStateComp from './Components/UseStateComp';
-import Form from './Components/Form';
-import Form2 from './Components/form2';
-import Hero from './Components/Hero';
-import Login from './Components/LogInComponents/Login';
-import SignUp from './Components/LogInComponents/SignUp';
-import LoginSignUp from './Components/LogInComponents/LoginSignUp';
-import Time from './Components/LogInComponents/Time';
+
 
 function App() {
   return (
     <>
-      {/* <Navigation elOne="About" elTwo='Projects' elThree="Contact Me"/>
-      <ProfileCard />
-      <Button /> */}
-
-      {/* <UseStateComp />
-      <Form />
-      <Form2 /> */}
-
-      {/* <Login />
-      <SignUp />
-      <br /> */}
-
-      {/* <LoginSignUp /> */}
-      <Time />
+      
+     
     </>
   );
 }
