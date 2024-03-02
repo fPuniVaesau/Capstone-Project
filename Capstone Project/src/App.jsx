a8928b59d324111469b3a19a5c36e35ecaa5131a
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-02 12:48:31 -0800

    working on the base components for the capstone project.

--- Capstone Project/src/App.jsx
+++ Capstone Project/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         profilePic={"https://images.unsplash.com/photo-1703533136832-2e91ef1a746f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI4fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D"}
         name='Filo Puni Vaesau'
         role={'Digital Craftsman | Full Stack Developer'}
-        bio={""}
+        bio={"You shall, I question not, find a way to the top if you diligently seek for it; for nature hath placed nothing so high that it is out of the reach of industry and valor. -Alexander The Great"}
       />
     </>
   );
