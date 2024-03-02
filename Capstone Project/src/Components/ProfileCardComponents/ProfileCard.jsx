df3958ce46b3561e6ad37db4187767dc31105ba9
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-02 12:40:26 -0800

    working on the base components for the capstone project.

--- Capstone Project/src/Components/ProfileCardComponents/ProfileCard.jsx
+++ Capstone Project/src/Components/ProfileCardComponents/ProfileCard.jsx
@@ -1,8 +1,9 @@
-export default function ProfileCard({name, role, bio, location, banner}){
+export default function ProfileCard({profilePic, name, role, bio, location, banner}){
     return(
         <div>
-            <img src="" alt="" />
-            <h2></h2>
+            <img src={profilePic} alt="Profile Picture" />
+            <h2>{name}</h2>
+
         </div>
     )
 }
