6517d06b05253d1e6664c8318319eca4fa0eee3d
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-03 13:34:38 -0800

    working on the Main section for the web app.

--- Capstone Project/src/Components/MainComponents/HeroComponents/Hero.jsx
+++ Capstone Project/src/Components/MainComponents/HeroComponents/Hero.jsx
@@ -2,7 +2,7 @@ import styles from "./Hero.module.css"
 export default function Hero({title, imgUrl}){
 
     return(
-        <div>
+        <div className={styles.heroContainer}>
             <h1>{title}</h1>
             <img className={styles.heroImage}  src={imgUrl} alt="Hero Image" />
         </div>
