9c65122420171b8a19d44ac24f0794281d96bcc3
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-03 14:49:31 -0800

    working on the Main section for the web app.

--- Capstone Project/src/Components/MainComponents/Main.jsx
+++ Capstone Project/src/Components/MainComponents/Main.jsx
@@ -20,7 +20,7 @@ export default function Main() {
       <Hero
         title={'What is the Backend in Web Development?'}
         imgUrl={
-          'https://images.unsplash.com/photo-1639322537231-2f206e06af84?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          'https://images.unsplash.com/photo-1705911292196-d64e7742b4f9?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         }
       />
     </div>
