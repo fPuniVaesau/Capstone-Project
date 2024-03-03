451233c26b78b42b5c5bef97005ab5b642a60fd7
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-02 19:39:31 -0800

    working on a form component for the webpage.

--- Capstone Project/src/Components/MainComponents/FormComponents/Form.jsx
+++ Capstone Project/src/Components/MainComponents/FormComponents/Form.jsx
@@ -2,17 +2,21 @@ import React, { useState } from 'react';
 
 export default function Form(){
     let [name, setName] = useState('');
-    
+    let [guestList, setGuestList] = ([])
+
+    let listenForChange = (e) =>{
+        setName(e.target.value);
+    }
     return(
         <div>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
                 <h2>Grandpas 100th Birthday</h2>
                 <label htmlFor="">Guest Name: </label>
-                <input type="text" />
+                <input type="text" value={name} onChange={listenForChange} />
                 <button>Add</button>
             </form>
             <div>
-                {}
+                {name}
             </div>
         </div>
     )
