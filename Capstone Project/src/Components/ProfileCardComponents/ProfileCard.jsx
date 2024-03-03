d8d73b93892d85fbe8e709c287e29902b1e70269
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-02 17:45:33 -0800

    working on the base components for the capstone project.

--- Capstone Project/src/Components/ProfileCardComponents/ProfileCard.jsx
+++ Capstone Project/src/Components/ProfileCardComponents/ProfileCard.jsx
@@ -1,13 +1,14 @@
+import styles from "./ProfileCard.module.css"
 export default function ProfileCard({profilePic, name, role, bio, location, CardBanner}){
     return(
         <div>
-            <img src={profilePic} alt="Profile Picture" />
-            <h1>{name}</h1>
-            <h2>{role}</h2>
-            <h3>{location}</h3>
-            <p>{bio}</p>
-            <img src={CardBanner} alt="Profile Banner" />
-            <button>Follow+</button>
+            <img className={styles.profileImage} src={profilePic} alt="Profile Picture" />
+            <h1 className={styles.name}>{name}</h1>
+            <h2 className={styles.role}>{role}</h2>
+            <h3 className={styles.location}>{location}</h3>
+            <p className={styles.bio}>{bio}</p>
+            <img className={styles.banner} src={CardBanner} alt="Profile Banner" />
+            <button className={styles.button}>Follow+</button>
         </div>
     )
 }
