221b45baeb0fddb2fc3bb6529ab23e2e01f65279
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-03 15:01:19 -0800

    working on the Main section for the web app.

--- Capstone Project/src/App.jsx
+++ Capstone Project/src/App.jsx
@@ -2,14 +2,11 @@ import './styles.css';
 import NavBar from './Components/HeaderComponents/NavBarComponents/NavBar';
 import Main from './Components/MainComponents/Main';
 
-
-
 export default function App() {
   return (
     <>
       <NavBar />
       <Main />
-      
     </>
   );
 }
