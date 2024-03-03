4ffdf632f162bf6631cba420b172c052c666478e
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-02 19:36:23 -0800

    working on a form component for the webpage.

--- Capstone Project/src/Components/MainComponents/FormComponents/Form.jsx
+++ Capstone Project/src/Components/MainComponents/FormComponents/Form.jsx
@@ -6,7 +6,13 @@ export default function Form(){
         <div>
             <form action="">
                 <h2>Grandpas 100th Birthday</h2>
+                <label htmlFor="">Guest Name: </label>
+                <input type="text" />
+                <button>Add</button>
             </form>
+            <div>
+                {}
+            </div>
         </div>
     )
 }
