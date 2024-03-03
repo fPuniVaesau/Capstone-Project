0c4460b8dbbdd6148b9cf7a310458af0d0e19362
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-02 19:51:12 -0800

    working on a form component for the webpage.

--- Capstone Project/src/App.jsx
+++ Capstone Project/src/App.jsx
@@ -2,24 +2,24 @@ import './styles.css';
 import NavBar from './Components/HeaderComponents/NavBarComponents/NavBar';
 import Form from './Components/MainComponents/FormComponents/Form';
 import ProfileCard from './Components/ProfileCardComponents/ProfileCard';
-function App() {
+
+export default function App() {
   return (
     <>
       <NavBar />
 
       <ProfileCard
-        profilePic={"https://images.unsplash.com/photo-1703533136832-2e91ef1a746f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI4fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D"}
+        profilePic={
+          'https://images.unsplash.com/photo-1703533136832-2e91ef1a746f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI4fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D'
+        }
         location={'San Francisco, Ca'}
         name='Filo Puni Vaesau'
         role={'Digital Craftsman | Full Stack Developer'}
-        bio={'"You shall, I question not, find a way to the top if you diligently seek for it; for nature hath placed nothing so high that it is out of the reach of industry and valor. -Alexander The Great"'}
-        CardBanner={"https://i.redd.it/sni0noeuz6311.gif"}
+        bio={
+          '"You shall, I question not, find a way to the top if you diligently seek for it; for nature hath placed nothing so high that it is out of the reach of industry and valor. -Alexander The Great"'
+        }
+        CardBanner={'https://i.redd.it/sni0noeuz6311.gif'}
       />
-
     </>
   );
 }
-
-export default App;
-
-
