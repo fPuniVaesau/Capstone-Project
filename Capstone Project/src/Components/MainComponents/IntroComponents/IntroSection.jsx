1a7430dfc9443211689c376c4a9cd15e009873a4
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-04 19:32:37 -0800

    working on the intro section for the main content.

--- Capstone Project/src/Components/MainComponents/IntroComponents/IntroSection.jsx
+++ Capstone Project/src/Components/MainComponents/IntroComponents/IntroSection.jsx
@@ -2,7 +2,7 @@ import styles from "./IntroSection.module.css"
 export default function IntroSection(){
     return(
         <div>
-            <h2 className="">Intro</h2>
+            <h2 className={styles.title}>Intro</h2>
         </div>
     )
 }
