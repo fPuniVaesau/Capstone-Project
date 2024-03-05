fbc12582c96f9040ed33375331f31e9dd666f9ff
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-04 19:31:39 -0800

    working on the intro section for the main content.

--- Capstone Project/src/Components/MainComponents/Main.jsx
+++ Capstone Project/src/Components/MainComponents/Main.jsx
@@ -21,6 +21,7 @@ export default function Main() {
         />
         <Hero
           title={'What is the Backend?'}
+          subTitle={"when it comes to web development?"}
           imgUrl={
             'https://images.unsplash.com/photo-1705911292196-d64e7742b4f9?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
           }
