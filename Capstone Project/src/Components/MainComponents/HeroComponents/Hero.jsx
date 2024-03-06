7da3a8341cece619f8b8627fd01c643c29951af5
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-05 18:41:34 -0800

    working on the intro section for the main content.

--- Capstone Project/src/Components/MainComponents/HeroComponents/Hero.jsx
+++ Capstone Project/src/Components/MainComponents/HeroComponents/Hero.jsx
@@ -3,7 +3,6 @@ export default function Hero({ title, imgUrl, subTitle }) {
   return (
     <div className={styles.heroContainer}>
       <h1 className={styles.mainTitle}>{title}</h1>
-      <h2>{subTitle}</h2>
       {/* <img className={styles.heroImage}  src={imgUrl} alt="Hero Image" /> */}
     </div>
   );
