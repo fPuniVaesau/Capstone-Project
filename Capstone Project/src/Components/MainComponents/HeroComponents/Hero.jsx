26a7544e45159a69e46e0874e7d64deba9cd9b56
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-05 18:45:49 -0800

    working on the intro section for the main content.

--- Capstone Project/src/Components/MainComponents/HeroComponents/Hero.jsx
+++ Capstone Project/src/Components/MainComponents/HeroComponents/Hero.jsx
@@ -1,8 +1,8 @@
 import styles from './Hero.module.css';
-export default function Hero({ title}) {
+export default function Hero() {
   return (
     <div className={styles.heroContainer}>
-      <h1 className={styles.mainTitle}>{title}</h1>
+      <h1 className={styles.mainTitle}>The Backend</h1>
     </div>
   );
 }
