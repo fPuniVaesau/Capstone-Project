0215495493565c2f4543c0c9ead4aa87cbc981b9
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-06 17:02:17 -0800

    working on the intro section for the main content.

--- Capstone Project/src/Components/MainComponents/IntroComponents/IntroSection.jsx
+++ Capstone Project/src/Components/MainComponents/IntroComponents/IntroSection.jsx
@@ -4,7 +4,7 @@ export default function IntroSection(){
         <div>
             <h2 className={styles.title}>Intro</h2>
             <div>
-                
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sapiente possimus rem odio quia minus nulla dolor temporibus commodi nostrum. Odio cupiditate optio temporibus ullam magni minima necessitatibus adipisci fugit architecto, facilis officia deleniti atque sed magnam eius dolorem hic sunt modi aperiam cumque dolores. Illum, quod modi. Quas, molestiae?</p>
             </div>
         </div>
     )
