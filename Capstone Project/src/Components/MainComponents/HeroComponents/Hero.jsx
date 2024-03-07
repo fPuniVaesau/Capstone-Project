5e7838b0c577bac270fe7f9313f2907331248e30
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-06 17:29:14 -0800

    working on the intro section for the main content.

--- Capstone Project/src/Components/MainComponents/HeroComponents/Hero.jsx
+++ Capstone Project/src/Components/MainComponents/HeroComponents/Hero.jsx
@@ -3,7 +3,19 @@ export default function Hero() {
   return (
     <div className={styles.heroContainer}>
       <h1 className={styles.mainTitle}>The Backend</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, delectus quisquam voluptatibus nulla eos vel soluta, quaerat temporibus ad dicta, doloribus officia cum. Repudiandae dolor laborum voluptas, itaque, tenetur quos aliquid non architecto possimus quo sapiente ipsum deserunt delectus numquam modi, perferendis in reiciendis ratione? Praesentium voluptatibus modi ad repellendus distinctio illum totam quis rerum! Iure inventore mollitia labore quo porro quod quis similique a repudiandae et impedit tempora, accusamus adipisci dolorem animi modi. Iure corrupti dicta repudiandae officiis sit amet, magni, ab ullam at eaque molestias sunt, vel ut enim facere fugit quisquam! Nostrum laboriosam praesentium aperiam dignissimos iusto.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia,
+        delectus quisquam voluptatibus nulla eos vel soluta, quaerat temporibus
+        ad dicta, doloribus officia cum. Repudiandae dolor laborum voluptas,
+        itaque, tenetur quos aliquid non architecto possimus quo sapiente ipsum
+        deserunt delectus numquam modi, perferendis in reiciendis ratione?
+        Praesentium voluptatibus modi ad repellendus distinctio illum totam quis
+        rerum! Iure inventore mollitia labore quo porro quod quis similique a
+        repudiandae et impedit tempora, accusamus adipisci dolorem animi modi.
+        Iure corrupti dicta repudiandae officiis sit amet, magni, ab ullam at
+        eaque molestias sunt, vel ut enim facere fugit quisquam! Nostrum
+        laboriosam praesentium aperiam dignissimos iusto.
+      </p>
     </div>
   );
 }
