2b1773bae3f020970d070bb1fd35e7c13bb4f646
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-06 17:29:47 -0800

    working on the intro section for the main content.

--- Capstone Project/src/Components/HeaderComponents/NavBarComponents/NavBar.jsx
+++ Capstone Project/src/Components/HeaderComponents/NavBarComponents/NavBar.jsx
@@ -1,12 +1,16 @@
-import styles from './NavBar.module.css'
+import styles from './NavBar.module.css';
 
-export default function NavBar(){
-    const navItems = ['About', 'Projects', 'Join']
-    return(
-        <div>
-            <ul className={styles.ul}>
-                {navItems.map(item=>(<li className={styles.listItem} key={item}>{item}</li>))}
-            </ul>
-        </div>
-    )
+export default function NavBar() {
+  const navItems = ['About', 'Projects', 'Join'];
+  return (
+    <div>
+      <ul className={styles.ul}>
+        {navItems.map((item) => (
+          <li className={styles.listItem} key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
