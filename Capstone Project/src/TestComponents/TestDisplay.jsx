92fb111bebb585518d8ea076aba6344bcda0117e
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-08 12:48:43 -0800

    working on the intro section for the main content.

--- Capstone Project/src/TestComponents/TestDisplay.jsx
+++ Capstone Project/src/TestComponents/TestDisplay.jsx
@@ -2,7 +2,7 @@ import styles from './TestDisplay.module.css';
 export default function TestDisplay({ guestList }) {
   return (
     <div className={styles.container}>
-      <div className={styles.taskContainer}>
+      <div>
         {guestList.map((person) => (
           <p>{person}</p>
         ))}
