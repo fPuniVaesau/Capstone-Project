45f7e00bce330d9e6aa3feea43a79c4cd181bcc1
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-07 16:50:15 -0800

    working on the intro section for the main content.

--- Capstone Project/src/TestComponents/Top.jsx
+++ Capstone Project/src/TestComponents/Top.jsx
@@ -1,14 +1,20 @@
 import TestForm from './TestForm';
 import TestDisplay from './TestDisplay';
 import { useState } from 'react';
+
 export default function Top() {
   const [name, setName] = useState('');
   const [guestlist, setGuestList] = useState([]);
 
   return (
     <>
-      <TestForm name={name} setName={setName} setGuestList={setGuestList} guestList={guestlist}/>
-      <TestDisplay guestList={guestlist}/>
+      <TestForm
+        name={name}
+        setName={setName}
+        setGuestList={setGuestList}
+        guestList={guestlist}
+      />
+      <TestDisplay guestList={guestlist} />
     </>
   );
 }
