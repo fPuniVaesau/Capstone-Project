3d51a6575aba0b55b621bd348821c1a9f0b2f78c
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-07 17:20:53 -0800

    working on the intro section for the main content.

--- Capstone Project/src/TestComponents/TestForm.jsx
+++ Capstone Project/src/TestComponents/TestForm.jsx
@@ -1,3 +1,5 @@
+import styles from "./TestForm.module.css"
+
 export default function TestForm({setName, name, setGuestList, guestList}) {
   let handleInput = (e) => {
     setName(e.target.value);
@@ -10,12 +12,12 @@ export default function TestForm({setName, name, setGuestList, guestList}) {
   };
 
   return (
-    <>
+    <div>
       <form action='' onSubmit={handleSubmit}>
         <label htmlFor=''>Name</label>
         <input type='text' onChange={handleInput} value={name} />
         <button>Show Name</button>
       </form>
-    </>
+    </div>
   );
 }
