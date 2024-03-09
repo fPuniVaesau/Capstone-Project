34e2c29abac75cfb3302464e87c0ac68ff94778c
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-08 16:09:57 -0800

    working on the intro section for the main content.

--- Capstone Project/src/TestComponents/TestDisplay.jsx
+++ Capstone Project/src/TestComponents/TestDisplay.jsx
@@ -5,7 +5,11 @@ export default function TestDisplay({ taskList }) {
       <div>
         {taskList.map((task) => (
           <div className={styles.taskContainer}>
-            <p>{task}</p>
+            <p className={styles.taskTitle}>{task}</p>
+            <div>
+                <button>Complete</button>
+                <button>X</button>
+            </div>
           </div>
         ))}
       </div>
