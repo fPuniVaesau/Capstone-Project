a194755e583de33d0a52f0d8a60ff466cf706aac
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-09 15:28:48 -0800

    working on styling some componenets.

--- Capstone Project/src/TestComponents/TestForm.jsx
+++ Capstone Project/src/TestComponents/TestForm.jsx
@@ -14,7 +14,7 @@ export default function TestForm({ setName, name, setTaskList, taskList }) {
   return (
     <div className={styles.formContainer}>
       <form className={styles.form} onSubmit={handleSubmit}>
-        <label htmlFor=''>Task: </label>
+        <label htmlFor=''>⚡️</label>
         <input
           className={styles.inputField}
           type='text'
