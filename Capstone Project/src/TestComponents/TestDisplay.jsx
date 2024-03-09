ecf7d775831478af4fc9b9fb88098a4912d992f9
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-08 16:47:45 -0800

    working on the intro section for the main content.

--- Capstone Project/src/TestComponents/TestDisplay.jsx
+++ Capstone Project/src/TestComponents/TestDisplay.jsx
@@ -1,14 +1,17 @@
 import styles from './TestDisplay.module.css';
 export default function TestDisplay({ taskList }) {
+    const handleDelete = () => {
+        console.log(taskList)
+    }
   return (
     <div className={styles.container}>
       <div>
         {taskList.map((task) => (
           <div className={styles.taskContainer}>
-            <p className={styles.taskTitle}>{task}</p>
+            <p key={task} className={styles.taskTitle}>{task}</p>
             <div className={styles.buttonContainer}>
               <button>Complete</button>
-              <button>X</button>
+              <button onClick={handleDelete}>X</button>
             </div>
           </div>
         ))}
