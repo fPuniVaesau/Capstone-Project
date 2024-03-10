078d66a4ec777a7ca2b8effc8c5bdbf6127cbecd
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-09 18:41:17 -0800

    working on styling some componenets.

--- Capstone Project/src/TestComponents/ButtonComponents.jsx
+++ Capstone Project/src/TestComponents/ButtonComponents.jsx
@@ -1,6 +1,10 @@
+import styles from "./ButtonComponents.module.css"
 
-export default function ButtonComponents(){
-    return(
-        
-    )
+export default function ButtonComponents() {
+  return (
+    <div className={styles.buttonContainer}>
+      <button className={styles.completeAndClose}>Complete</button>
+      <button className={styles.completeAndClose}>X</button>
+    </div>
+  );
 }
--- Capstone Project/src/TestComponents/TestDisplay.jsx
+++ Capstone Project/src/TestComponents/TestDisplay.jsx
@@ -1,3 +1,4 @@
+import ButtonComponents from './ButtonComponents';
 import ListItems from './ListItems';
 import styles from './TestDisplay.module.css';
 export default function TestDisplay({ taskList, setTaskList }) {
@@ -5,10 +6,7 @@ export default function TestDisplay({ taskList, setTaskList }) {
     <div className={styles.container}>
       <ListItems taskList={taskList} setTaskList={setTaskList} />
 
-      <div className={styles.buttonContainer}>
-        <button className={styles.completeAndClose}>Complete</button>
-        <button className={styles.completeAndClose}>X</button>
-      </div>
+      <ButtonComponents />
     </div>
   );
 }
