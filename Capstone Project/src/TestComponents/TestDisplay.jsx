e234944ca9c0b0d6bb6c8a55127878715a0d6224
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-09 18:36:09 -0800

    working on styling some componenets.

--- Capstone Project/src/TestComponents/TestDisplay.jsx
+++ Capstone Project/src/TestComponents/TestDisplay.jsx
@@ -1,20 +1,13 @@
 import ListItems from './ListItems';
 import styles from './TestDisplay.module.css';
 export default function TestDisplay({ taskList, setTaskList }) {
-  const handleDelete = (task) => {
-    console.log(task);
-  };
   return (
     <div className={styles.container}>
       <ListItems taskList={taskList} setTaskList={setTaskList} />
+
       <div className={styles.buttonContainer}>
         <button className={styles.completeAndClose}>Complete</button>
-        <button
-          className={styles.completeAndClose}
-          onClick={() => handleDelete(task)}
-        >
-          X
-        </button>
+        <button className={styles.completeAndClose}>X</button>
       </div>
     </div>
   );
