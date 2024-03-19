b38c9b807419c20617fce905c6be63f2f1b88e70
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-19 13:35:26 -0700

    working on components and conditional rendering.

--- Capstone Project/src/TestComponents/ListItems.jsx
+++ Capstone Project/src/TestComponents/ListItems.jsx
@@ -10,7 +10,7 @@ export default function ListItems({
 }) {
   let handleDelete = (task) => {
     setTaskList(taskList.filter((taskInList) => taskInList !== task));
-    setTaskCount(task.taskComplete ? taskCount : taskCount - 1);
+    setTaskCount(taskCount - 1);
     console.log(task);
   };
 
@@ -21,7 +21,6 @@ export default function ListItems({
         : object
     );
     setTaskList(mapped);
-    setTaskCount(taskCount - 1);
     // console.log(taskList)
   };
 
