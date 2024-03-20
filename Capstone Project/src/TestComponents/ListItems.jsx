86a7d5c17ed6bbe61accc012e5e38bcd4ea3d9a3
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-20 14:39:32 -0700

    working on components and conditional rendering.

--- Capstone Project/src/TestComponents/ListItems.jsx
+++ Capstone Project/src/TestComponents/ListItems.jsx
@@ -21,7 +21,8 @@ export default function ListItems({
         : object
     );
     setTaskList(mapped);
-    let itemsCompleted = taskList.map(completed => completed.taskComplete).length
+
+    let itemsCompleted = taskList.filter(completed => completed.taskComplete).length
     console.log(itemsCompleted);
   };
 
--- Capstone Project/src/TestComponents/Top.jsx
+++ Capstone Project/src/TestComponents/Top.jsx
@@ -7,7 +7,7 @@ export default function Top() {
   const [taskItem, setTaskItem] = useState({ name: '', taskComplete: false });
   const [taskList, setTaskList] = useState([]);
   const [taskCount, setTaskCount] = useState(0);
-  
+  const [taskDone, setTaskDone] = useState(0)
   return (
     <div className={styles.container}>
       <div className={styles.titleContainer}>
@@ -22,6 +22,8 @@ export default function Top() {
         taskList={taskList}
         taskCount={taskCount}
         setTaskCount={setTaskCount}
+        taskDone
+        setTaskDone
       />
       <ListItems
         className={styles.test}
@@ -31,6 +33,8 @@ export default function Top() {
         setTaskList={setTaskList}
         taskCount={taskCount}
         setTaskCount={setTaskCount}
+        taskDone
+        setTaskDone
       />
     </div>
   );
