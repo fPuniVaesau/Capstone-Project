47d116731dbfbd34d8db3015ae055537b7c9c466
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-28 16:55:30 -0700

    Working on the card component for the application.

--- Capstone Project/src/App.jsx
+++ Capstone Project/src/App.jsx
@@ -3,7 +3,15 @@ import './styles.css';
 export default function App() {
   return (
     <>
-  
+      <header>
+        <nav>
+          <ul>
+            <li>About</li>
+            <li>Project</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 }
