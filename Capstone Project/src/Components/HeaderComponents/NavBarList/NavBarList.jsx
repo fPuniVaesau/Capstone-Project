5e4b6d969851fb6430424994eb67bc31949c2f58
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-28 17:25:28 -0700

    working on the header of the project.

--- Capstone Project/src/Components/HeaderComponents/NavBarList/NavBarList.jsx
+++ Capstone Project/src/Components/HeaderComponents/NavBarList/NavBarList.jsx
@@ -1,4 +1,13 @@
 
 export default function NavBarList(){
-    
+
+    return(
+        <nav>
+          <ul>
+            <li>About</li>
+            <li>Project</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+    )
 }
