ebca0de452ef3e9122ffa5e8329f400f63ed17ba
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-28 18:17:44 -0700

    working on the header of the project.

--- Capstone Project/src/Components/MainContentComponents/MainContent.jsx
+++ Capstone Project/src/Components/MainContentComponents/MainContent.jsx
@@ -2,12 +2,12 @@ import ProfileCard from "./ProfileCardComponents/ProfileCard";
 export default function MainContent(){
     return(
         <ProfileCard
-            imgUrl={}
+            imgUrl={''}
             name={'Filo Puni Vaesau'}
             role={'Digital Craftsman | Fullstack Developer'}
             location={'San Francisco, Ca'}
             bio={'You will never discover new oceans if you do not have courage to lose sight of the shore.'}
-            banner={}
+            banner={''}
         />
     );
 }
