9faf4c2ed92ef4347bb2728c8cdd8242de13bb29
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-28 17:29:04 -0700

    working on the header of the project.

--- Capstone Project/src/Components/HeaderComponents/MainHeader.jsx
+++ Capstone Project/src/Components/HeaderComponents/MainHeader.jsx
@@ -1,8 +1,10 @@
 import NavBarLogo from "./NavBarLogoComp/NavBarLogo"
+import NavBarList from "./NavBarList/NavBarList"
 export default function MainHeader(){
     return(
         <header>
             <NavBarLogo imgUrl={'https://www.pngall.com/wp-content/uploads/15/Snorlax-PNG-Cutout.png'} altDiscription={'Snorlax Placeholder Logo'} />
+            <NavBarList liOne={'About'} liTwo={'Projects'} liThree={'About'} />
         </header>
     )
 }
