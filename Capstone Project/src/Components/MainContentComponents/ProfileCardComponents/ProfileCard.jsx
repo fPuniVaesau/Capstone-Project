f87e9952b489709e32454a9c4c579e96e1098353
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-28 18:18:32 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/ProfileCardComponents/ProfileCard.jsx
+++ Capstone Project/src/Components/MainContentComponents/ProfileCardComponents/ProfileCard.jsx
@@ -1,13 +1,20 @@
-import styles from "./ProfileCard.module.css"
-export default function ProfileCard({imgUrl, name, role, location, bio, banner}){
-    return(
-        <div className={styles.container}>
-            <img src={imgUrl} alt="Profile Picture" />
-            <h2>{name}</h2>
-            <h3>{location}</h3>
-            <h3>{role}</h3>
-            <p>{bio}</p>
-            <img src={banner} alt="Profile Banner" />
-        </div>
-    );
+import styles from './ProfileCard.module.css';
+export default function ProfileCard({
+  imgUrl,
+  name,
+  role,
+  location,
+  bio,
+  banner,
+}) {
+  return (
+    <div className={styles.container}>
+      <img src={imgUrl} alt='Profile Picture' />
+      <h2>{name}</h2>
+      <h3>{location}</h3>
+      <h3>{role}</h3>
+      <p>{bio}</p>
+      <img src={banner} alt='Profile Banner' />
+    </div>
+  );
 }
