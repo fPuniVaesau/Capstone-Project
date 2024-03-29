3a86d7d64d7e5d5bfdef2d2aea8e3f8094b216c8
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-28 18:19:23 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/HeaderComponents/NavBarLogoComp/NavBarLogo.jsx
+++ Capstone Project/src/Components/HeaderComponents/NavBarLogoComp/NavBarLogo.jsx
@@ -1,10 +1,9 @@
-import styles from "./NavBarLogo.module.css"
+import styles from './NavBarLogo.module.css';
 
-export default function NavBarLogo({imgUrl, altDiscription}){
-
-    return(
-        <div>
-            <img src={imgUrl} alt={altDiscription} />
-        </div>
-    )
+export default function NavBarLogo({ imgUrl, altDiscription }) {
+  return (
+    <div>
+      <img src={imgUrl} alt={altDiscription} />
+    </div>
+  );
 }
