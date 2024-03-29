c35d44485329273bd87511f010c2c2dc6f350584
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-28 18:06:30 -0700

    working on the header of the project.

--- Capstone Project/src/App.jsx
+++ Capstone Project/src/App.jsx
@@ -1,5 +1,4 @@
 import './styles.css';
-import styles from './App.modules.css'
 import MainHeader from './Components/HeaderComponents/MainHeader';
 import MainContent from './Components/MainContentComponents/MainContent';
 export default function App() {
