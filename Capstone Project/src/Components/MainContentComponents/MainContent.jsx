f57f560b9081a60a3115361ccf690c874dcd9479
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-29 16:07:42 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/MainContent.jsx
+++ Capstone Project/src/Components/MainContentComponents/MainContent.jsx
@@ -1,24 +1,27 @@
 import ProfileCard from './ProfileCardComponents/ProfileCard';
 import Title from './TitleComponents/Title';
 
-
-import styles from "./MainContent.module.css";
+import styles from './MainContent.module.css';
 export default function MainContent() {
   return (
     <div className={styles.mainContainer}>
-        <ProfileCard
-      imgUrl={""}
-      name={'Filo Puni Vaesau'}
-      role={'Digital Craftsman | Fullstack Developer'}
-      location={'San Francisco, Ca'}
-      bio={
-        'You will never discover new oceans if you do not have courage to lose sight of the shore.'
-      }
-      banner={'https://i.pinimg.com/originals/d4/2b/aa/d42baaecec787840fd2b41fe84c040d8.gif'}
-    />
+      
+      <ProfileCard
+        imgUrl={''}
+        name={'Filo Puni Vaesau'}
+        role={'Digital Craftsman | Fullstack Developer'}
+        location={'San Francisco, Ca'}
+        bio={
+          'You will never discover new oceans if you do not have courage to lose sight of the shore.'
+        }
+        banner={
+          'https://i.pinimg.com/originals/d4/2b/aa/d42baaecec787840fd2b41fe84c040d8.gif'
+        }
+      />
 
-      <Title mainTitle={"Getting to know the Backend!"}/>
+      <Title mainTitle={'Getting to know the Backend!'} />
+
+
     </div>
-
   );
 }
