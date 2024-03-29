e097d96108a72d671b78d770239dd27649ac3a7b
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-28 19:39:43 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/TitleComponents/Title.jsx
+++ Capstone Project/src/Components/MainContentComponents/TitleComponents/Title.jsx
@@ -2,8 +2,8 @@ import styles from "./Title.module.css"
 
 export default function Title({mainTitle}){
     return(
-        <div>
-            <h1>{mainTitle}</h1>
+        <div className={styles.titleContainer}>
+            <h1 className={styles.mainTitle}>{mainTitle}</h1>
         </div>
     )
 }
