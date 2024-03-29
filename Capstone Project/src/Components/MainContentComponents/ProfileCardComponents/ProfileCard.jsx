48c934d73bbb0d39b2ba0ca62078092f954fed6f
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-28 19:28:40 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/ProfileCardComponents/ProfileCard.jsx
+++ Capstone Project/src/Components/MainContentComponents/ProfileCardComponents/ProfileCard.jsx
@@ -11,9 +11,9 @@ export default function ProfileCard({
     <div className={styles.container}>
       <img className={styles.profileImg} src={imgUrl} alt='Profile Picture' />
       <h2 className={styles.profileName}>{name}</h2>
-      <h3>{location}</h3>
-      <h3>{role}</h3>
-      <p>{bio}</p>
+      <h3 className={styles.role}>{role}</h3>
+      <h3 className={styles.location}>{location}</h3>
+      <p className={styles.bio}>{bio}</p>
       <img className={styles.profileBanner} src={banner} alt='Profile Banner' />
     </div>
   );
