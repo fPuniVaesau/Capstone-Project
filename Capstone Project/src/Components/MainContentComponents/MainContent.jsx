2fd62fb830e9cea59eb9332a84377529758978d4
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-29 16:08:23 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/MainContent.jsx
+++ Capstone Project/src/Components/MainContentComponents/MainContent.jsx
@@ -5,9 +5,9 @@ import styles from './MainContent.module.css';
 export default function MainContent() {
   return (
     <div className={styles.mainContainer}>
-      
+
       <ProfileCard
-        imgUrl={''}
+        imgUrl={'https://avatars.githubusercontent.com/u/126365920?v=4'}
         name={'Filo Puni Vaesau'}
         role={'Digital Craftsman | Fullstack Developer'}
         location={'San Francisco, Ca'}
