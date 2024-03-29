4920e8bb3e0c7423b3540f40f836e02b399b25d0
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-28 18:19:09 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/HeaderComponents/NavBarList/NavBarList.jsx
+++ Capstone Project/src/Components/HeaderComponents/NavBarList/NavBarList.jsx
@@ -1,13 +1,12 @@
-import styles from "./NavBarList.module.css"
-export default function NavBarList({liOne, liTwo, liThree}){
-
-    return(
-        <nav>
-          <ul>
-            <li>{liOne}</li>
-            <li>{liTwo}</li>
-            <li>{liThree}</li>
-          </ul>
-        </nav>
-    )
+import styles from './NavBarList.module.css';
+export default function NavBarList({ liOne, liTwo, liThree }) {
+  return (
+    <nav>
+      <ul>
+        <li>{liOne}</li>
+        <li>{liTwo}</li>
+        <li>{liThree}</li>
+      </ul>
+    </nav>
+  );
 }
--- Capstone Project/src/Components/MainContentComponents/MainContent.jsx
+++ Capstone Project/src/Components/MainContentComponents/MainContent.jsx
@@ -1,13 +1,15 @@
-import ProfileCard from "./ProfileCardComponents/ProfileCard";
-export default function MainContent(){
-    return(
-        <ProfileCard
-            imgUrl={''}
-            name={'Filo Puni Vaesau'}
-            role={'Digital Craftsman | Fullstack Developer'}
-            location={'San Francisco, Ca'}
-            bio={'You will never discover new oceans if you do not have courage to lose sight of the shore.'}
-            banner={''}
-        />
-    );
+import ProfileCard from './ProfileCardComponents/ProfileCard';
+export default function MainContent() {
+  return (
+    <ProfileCard
+      imgUrl={''}
+      name={'Filo Puni Vaesau'}
+      role={'Digital Craftsman | Fullstack Developer'}
+      location={'San Francisco, Ca'}
+      bio={
+        'You will never discover new oceans if you do not have courage to lose sight of the shore.'
+      }
+      banner={''}
+    />
+  );
 }
