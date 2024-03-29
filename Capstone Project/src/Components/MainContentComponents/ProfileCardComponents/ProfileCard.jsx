79cbaa9c71c2755d00418d6dd46e704dab33191a
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-28 18:12:59 -0700

    working on the header of the project.

--- Capstone Project/src/Components/MainContentComponents/ProfileCardComponents/ProfileCard.jsx
+++ Capstone Project/src/Components/MainContentComponents/ProfileCardComponents/ProfileCard.jsx
@@ -1,10 +1,12 @@
 import styles from "./ProfileCard.module.css"
-export default function ProfileCard(){
+export default function ProfileCard({imgUrl, name, role, location, bio}){
     return(
         <div className={styles.container}>
-            <img src="" alt="" />
-            <h2>Filo Puni Vaesau</h2>
-            <h3></h3>
+            <img src={imgUrl} alt="" />
+            <h2>{name}</h2>
+            <h3>{location}</h3>
+            <h3>{role}</h3>
+            <p>{bio}</p>
         </div>
     );
 }
