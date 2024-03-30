97486ff010f7dfb0882465012b730999b3cf66c7
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-30 14:24:23 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/MainContent.jsx
+++ Capstone Project/src/Components/MainContentComponents/MainContent.jsx
@@ -6,24 +6,26 @@ import styles from './MainContent.module.css';
 export default function MainContent() {
   return (
     <div className={styles.mainContainer}>
-      <ProfileCard
-        imgUrl={'https://avatars.githubusercontent.com/u/126365920?v=4'}
-        name={'Filo Puni Vaesau'}
-        role={'Digital Craftsman | Fullstack Developer'}
-        location={'San Francisco, Ca'}
-        bio={
-          'You will never discover new oceans if you do not have courage to lose sight of the shore.'
-        }
-        banner={
-          'https://i.pinimg.com/originals/d4/2b/aa/d42baaecec787840fd2b41fe84c040d8.gif'
-        }
-      />
+      <div>
+        <ProfileCard
+          imgUrl={'https://avatars.githubusercontent.com/u/126365920?v=4'}
+          name={'Filo Puni Vaesau'}
+          role={'Digital Craftsman | Fullstack Developer'}
+          location={'San Francisco, Ca'}
+          bio={
+            'You will never discover new oceans if you do not have courage to lose sight of the shore.'
+          }
+          banner={
+            'https://i.pinimg.com/originals/d4/2b/aa/d42baaecec787840fd2b41fe84c040d8.gif'
+          }
+        />
+      </div>
 
-      <Title mainTitle={'Getting to know the Backend!'} />
+      <div className={styles.two}>
+        <Title mainTitle={'Getting to know the Backend!'} />
+      </div>
 
-      <IntroductionSec />
-
-     
+      <IntroductionSec className={styles.test} />
     </div>
   );
 }
