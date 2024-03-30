926b56c710bb644db7e22d98e49bd447cac1786b
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-30 13:30:16 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/IntroComponents/Intro.jsx
+++ Capstone Project/src/Components/MainContentComponents/IntroComponents/Intro.jsx
@@ -1,9 +1,36 @@
+export default function Intro() {
+  
+    return (
+    <div>
+      <h3>What is Backend Development?</h3>
+      {/* <hr /> */}
+      <p>
+        When we use the web and access webpages and applications we are greeted
+        with beautiful layouts, fonts, colors, animations and more. All the hard
+        work that the developers had done is presented to us in a beautiful
+        package.
+      </p>
 
-export default function Intro(){
+      <p>
+        When we press buttons on the page, they do something, when we are
+        signing up for memberships or filling out a form, does something.
+        Whenever we interact with the web page, it does something. But what is
+        that something and how does it work?
+      </p>
 
-    return(
-        <>
-            <h2>This is a place holder</h2>
-        </>
-    )
+      <p>
+        Where does that information that I provided for that form go? Why is
+        information on a page being displayed when I press this button? These
+        are some of the questions that I asked when starting my journey into web
+        development.
+      </p>
+
+      <p>
+        If these are questions you are struggling with as well, then this is the
+        place for you.
+      </p>
+
+      <p>👋🏽 Welcome to learning about the Backend!</p>
+    </div>
+  );
 }
--- Capstone Project/src/Components/MainContentComponents/MainContent.jsx
+++ Capstone Project/src/Components/MainContentComponents/MainContent.jsx
@@ -1,6 +1,7 @@
 import ProfileCard from './ProfileCardComponents/ProfileCard';
 import Title from './TitleComponents/Title';
 
+
 import styles from './MainContent.module.css';
 export default function MainContent() {
   return (
@@ -20,30 +21,7 @@ export default function MainContent() {
 
       <Title mainTitle={'Getting to know the Backend!'} />
 
-      <div>
-        <h3>What is Backend Development?</h3>
-        {/* <hr /> */}
-        <p>
-          When we use the web and access webpages and applications we are
-          greeted with beautiful layouts, fonts, colors, animations and more.
-          All the hard work that the developers had done is presented to us in a
-          beautiful package.
-        </p>
-
-        <p>
-          When we press buttons on the page, they do something, when we are
-          signing up for memberships or filling out a form, does something.
-          Whenever we interact with the web page, it does something. But what is
-          that something and how does it work? 
-        </p>
-
-        <p>
-        Where does that information that
-          I provided for that form go? Why is information on a page being
-          displayed when I press this button? These are some of the questions
-          that I asked when starting my journey into web development.
-        </p>
-      </div>
+      
     </div>
   );
 }
