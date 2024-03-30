4a02e413cf69bc9dcbc2613ca1e8e04d1005d7e3
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-03-30 13:25:10 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/MainContent.jsx
+++ Capstone Project/src/Components/MainContentComponents/MainContent.jsx
@@ -5,7 +5,6 @@ import styles from './MainContent.module.css';
 export default function MainContent() {
   return (
     <div className={styles.mainContainer}>
-
       <ProfileCard
         imgUrl={'https://avatars.githubusercontent.com/u/126365920?v=4'}
         name={'Filo Puni Vaesau'}
@@ -20,17 +19,31 @@ export default function MainContent() {
       />
 
       <Title mainTitle={'Getting to know the Backend!'} />
-      
+
       <div>
         <h3>What is Backend Development?</h3>
         {/* <hr /> */}
-        <p>When we use the web and access webpages and applications we are greeted with beautiful layouts, fonts, colors, animations and more. All the hard work that the developers had done is presented to us in a beautiful package.</p>
+        <p>
+          When we use the web and access webpages and applications we are
+          greeted with beautiful layouts, fonts, colors, animations and more.
+          All the hard work that the developers had done is presented to us in a
+          beautiful package.
+        </p>
 
         <p>
-          
+          When we press buttons on the page, they do something, when we are
+          signing up for memberships or filling out a form, does something.
+          Whenever we interact with the web page, it does something. But what is
+          that something and how does it work? 
+        </p>
+
+        <p>
+        Where does that information that
+          I provided for that form go? Why is information on a page being
+          displayed when I press this button? These are some of the questions
+          that I asked when starting my journey into web development.
         </p>
       </div>
-
     </div>
   );
 }
