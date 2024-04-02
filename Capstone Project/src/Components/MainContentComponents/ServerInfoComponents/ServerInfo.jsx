ee0c27839611ab0b3eda6cc62cbd61783d30e0f8
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-02 14:01:39 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/ServerInfoComponents/ServerInfo.jsx
+++ Capstone Project/src/Components/MainContentComponents/ServerInfoComponents/ServerInfo.jsx
@@ -3,7 +3,7 @@ import styles from "./ServerInfo.module.css";
 export default function ServerInfo(){
   
   return(
-    <div>
+    <div className={styles.serverContainer}>
       <h1>Test</h1>
     </div>
   )
