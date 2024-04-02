659814667822bcc12189a862e389cf5568f5ea45
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-01 20:43:36 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/BackendArchitectureComponents/BACintroComponents/BACintro.jsx
+++ Capstone Project/src/Components/MainContentComponents/BackendArchitectureComponents/BACintroComponents/BACintro.jsx
@@ -9,26 +9,39 @@ export default function BACintro() {
       </p>
 
       <div className={styles.infoWrapper}>
-
         <div className={styles.info}>
           <h4>The Server</h4>
           <p>
             the computer or machine that will host your application and listens
-            for incoming request from the client
+            for incoming request from the client.
           </p>
+          <p>The server can be physical, virtual or on the cloud.</p>
         </div>
 
         <div className={styles.info}>
           <h4>The Application</h4>
-          <p></p>
+          <p>
+            the software or program that will run on the server which contains
+            the logic and functionality of the application.
+          </p>
+          <p>
+            It can be written in your choice of programming language and
+            framework that suits your needs.
+          </p>
         </div>
 
         <div className={styles.info}>
-          <h4>Database</h4>
-          <p></p>
+          <h4>The Database</h4>
+          <p>
+            Where you will store and manage your the data of your application.
+          </p>
+          <p>
+            It can be a relational database like MySQL or PostgreSQL, perhaps a
+            non-relational database like MongoDB or Firebase, or a combination
+            of both.
+          </p>
         </div>
       </div>
-
     </div>
   );
 }
