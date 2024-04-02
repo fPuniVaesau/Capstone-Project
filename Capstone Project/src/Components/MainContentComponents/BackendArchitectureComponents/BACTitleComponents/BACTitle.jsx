5cbba90b7fc2e1c223b10546804a4e6407be8b48
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-01 18:40:46 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/BackendArchitectureComponents/BACTitleComponents/BACTitle.jsx
+++ Capstone Project/src/Components/MainContentComponents/BackendArchitectureComponents/BACTitleComponents/BACTitle.jsx
@@ -1,5 +1,10 @@
 import styles from './BACTitle.module.css';
 
-export default function BACTitle(){
-  
+export default function BACTitle({Title}){
+
+  return(
+    <div>
+      <h3>{Title}</h3>
+    </div>
+  )
 }
