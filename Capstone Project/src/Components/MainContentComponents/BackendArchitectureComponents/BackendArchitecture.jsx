61df61f94d165836bbaa34e7c31006adff151a79
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-01 19:44:37 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/BackendArchitectureComponents/BackendArchitecture.jsx
+++ Capstone Project/src/Components/MainContentComponents/BackendArchitectureComponents/BackendArchitecture.jsx
@@ -1,10 +1,19 @@
 import BACTitle from "./BACTitleComponents/BACTitle";
+import styles from "./BackendArchitecture.module.css"
 
 export default function BackendArchitecture(){
 
   return(
-    <div>
+    <div className={styles.BACcontainer}>
       <BACTitle Title={"3 Main Components"} />
+
+      <div>
+        <p>When it comes to designing web applications, there are 3 things you must consider...</p>
+
+        <ul>
+
+        </ul>
+      </div>
     </div>
   )
 }
