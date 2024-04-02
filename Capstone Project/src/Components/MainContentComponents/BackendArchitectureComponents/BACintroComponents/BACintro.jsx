3f32b21e544054e5446c7244cb66e7fd800fe0df
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-01 20:12:02 -0700

    working on the profile card of the page for the main content.

---
+++ Capstone Project/src/Components/MainContentComponents/BackendArchitectureComponents/BACintroComponents/BACintro.jsx
@@ -0,0 +1,33 @@
+
+export default function BACintro(){
+
+  return(
+    <>
+      <div>
+        <p>
+          When it comes to designing web applications, there are 3 things you
+          must consider...
+        </p>
+      </div>
+
+      <div>
+        <div>
+          <h4>The Server</h4>
+          <p>
+            the computer or machine that will host your application and listens
+            for incoming request from the client
+          </p>
+        </div>
+
+        <div>
+          <h4>The Application</h4>
+          <p></p>
+        </div>
+
+        <div>
+          <h4>Database</h4>
+        </div>
+      </div>
+    </>
+  )
+}
--- Capstone Project/src/Components/MainContentComponents/BackendArchitectureComponents/BackendArchitecture.jsx
+++ Capstone Project/src/Components/MainContentComponents/BackendArchitectureComponents/BackendArchitecture.jsx
@@ -1,31 +1,12 @@
-import BACTitle from "./BACTitleComponents/BACTitle";
-import styles from "./BackendArchitecture.module.css"
+import BACTitle from './BACTitleComponents/BACTitle';
+import styles from './BackendArchitecture.module.css';
 
-export default function BackendArchitecture(){
-
-  
-
-  return(
+export default function BackendArchitecture() {
+  return (
     <div className={styles.BACcontainer}>
-      <BACTitle Title={"3 Main Components"} />
+      <BACTitle Title={'3 Main Components'} />
 
-      <div>
-        <p>When it comes to designing web applications, there are 3 things you must consider...</p>
-
-       <div>
-        <h4>Server</h4>
-        <p></p>
-       </div>
-
-       <div>
-        <h4>Application</h4>
-       </div>
-
-       <div>
-        <h4>Database</h4>
-       </div>
-       
-      </div>
+      
     </div>
-  )
+  );
 }
