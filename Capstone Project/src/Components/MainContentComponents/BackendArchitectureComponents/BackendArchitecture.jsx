13fb4c20ef88f7e3bd84389961fba00f39f5cb81
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-01 19:36:54 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/BackendArchitectureComponents/BackendArchitecture.jsx
+++ Capstone Project/src/Components/MainContentComponents/BackendArchitectureComponents/BackendArchitecture.jsx
@@ -1,5 +1,10 @@
 import BACTitle from "./BACTitleComponents/BACTitle";
 
 export default function BackendArchitecture(){
-  
+
+  return(
+    <div>
+      <BACTitle Title={"3 Main Components"} />
+    </div>
+  )
 }
