69a05419c03113cbe6b9b772e2bdca0c8d66c311
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-02 16:33:24 -0700

    working on the profile card of the page for the main content.

--- Capstone Project/src/Components/MainContentComponents/ServerInfoComponents/ServerInfo.jsx
+++ Capstone Project/src/Components/MainContentComponents/ServerInfoComponents/ServerInfo.jsx
@@ -5,6 +5,14 @@ export default function ServerInfo(){
   return(
     <div className={styles.serverContainer}>
       <h3>Server(s)</h3>
+
+      <div>
+        <p>For this project you are viewing, the server component will be handled using node.js & express.js</p>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <img className={styles.imageItem} src="https://cdn.dribbble.com/users/2520078/screenshots/11988083/media/16914c8400ad518b034fbe8102ba2d4f.gif" alt="server image" />
+      </div>
     </div>
   )
 }
