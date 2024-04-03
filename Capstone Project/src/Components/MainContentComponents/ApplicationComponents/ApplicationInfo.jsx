d5dbcedb778da9e98078ddb364061dd8e248c5fb
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-03 13:41:32 -0700

    working on the server side information that will be displayed on the web page.

--- Capstone Project/src/Components/MainContentComponents/ApplicationComponents/ApplicationInfo.jsx
+++ Capstone Project/src/Components/MainContentComponents/ApplicationComponents/ApplicationInfo.jsx
@@ -1,14 +1,17 @@
-import styles from "./ApplicationInfo.module.css"
+import styles from './ApplicationInfo.module.css';
 
-export default function ApplicationInfo(){
-
-  return(
+export default function ApplicationInfo() {
+  return (
     <div className={styles.applicationContainer}>
       <h3>Application(s)</h3>
 
       <div>
-        
+        <p>
+          The application is the software or program that you will be running on
+          the server. In this example the application would be this very
+          application that you are viewing.
+        </p>
       </div>
     </div>
-  )
+  );
 }
