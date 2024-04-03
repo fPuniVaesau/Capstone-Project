53ae2cde9aa9c810c61eaa80b6353e86cac23898
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-03 13:51:32 -0700

    working on the server side information that will be displayed on the web page.

--- Capstone Project/src/Components/MainContentComponents/ApplicationComponents/ApplicationInfo.jsx
+++ Capstone Project/src/Components/MainContentComponents/ApplicationComponents/ApplicationInfo.jsx
@@ -5,29 +5,29 @@ export default function ApplicationInfo() {
     <div className={styles.applicationContainer}>
       <h3>Application(s)</h3>
 
-      <div>
-        <h4>React.js</h4>
-        <p>
+      <div className={styles.appInfoContainer}>
+        <h4 className={styles.appSubtitle}>React.js</h4>
+        <p className={styles.appInfo}>
           React.js is a JavaScript library that is known for its declarative,
           component based approach to creating and building out user interfaces.
         </p>
 
-        <p>
+        <p className={styles.appInfo}>
           React allows developers to break down large complex applications into
           smaller re-usable components that are easier to manage.
         </p>
 
-        <p>
+        <p className={styles.appInfo}>
           A major advantage for the use of React.js is its virtual DOM, which is
           a light weight representation of the actual DOM.
         </p>
 
-        <p>
+        <p className={styles.appInfo}>
           This allows for components on the page to re-render when updated, not the entire web page. This improves overall performance and a better overall user experience.
         </p>
       </div>
 
-      <div>
+      <div className={}>
         <img src="" alt="" />
       </div>
     </div>
