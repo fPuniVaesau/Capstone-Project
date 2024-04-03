6bdf1068c841ff061922048d48a417df2dc8f99c
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-02 17:22:12 -0700

    working on the server side information that will be displayed on the web page.

--- Capstone Project/src/Components/MainContentComponents/DatabaseComponents/DatabaseInfo.jsx
+++ Capstone Project/src/Components/MainContentComponents/DatabaseComponents/DatabaseInfo.jsx
@@ -1,10 +1,25 @@
-import styles from "./DatabaseInfo.module.css"
+import styles from './DatabaseInfo.module.css';
 
-export default function DatabaseInfo(){
-
-  return(
+export default function DatabaseInfo() {
+  return (
     <div className={styles.databaseContainer}>
       <h3>Database(s)</h3>
+
+      <div>
+        <h4>MongoDB</h4>
+        <p>
+          The Database Layer MongoDB is a NoSQL database that offers a flexible
+          and scalable solution for storing data.
+        </p>
+        <p>
+          Unlike traditional relational databases, MongoDB uses a document-based
+          model, allowing developers to work with JSON-like documents.
+        </p>
+        <p>
+          This flexibility enables faster development cycles and easier schema
+          updates.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
