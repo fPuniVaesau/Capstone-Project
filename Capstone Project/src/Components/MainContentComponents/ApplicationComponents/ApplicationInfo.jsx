acfda223354a9c9fbdeb2b7c85c7812b703e4e3c
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-03 13:48:14 -0700

    working on the server side information that will be displayed on the web page.

--- Capstone Project/src/Components/MainContentComponents/ApplicationComponents/ApplicationInfo.jsx
+++ Capstone Project/src/Components/MainContentComponents/ApplicationComponents/ApplicationInfo.jsx
@@ -6,10 +6,24 @@ export default function ApplicationInfo() {
       <h3>Application(s)</h3>
 
       <div>
+        <h4>React.js</h4>
         <p>
-          The application is the software or program that you will be running on
-          the server. In this example the application would be this very
-          application that you are viewing.
+          React.js is a JavaScript library that is known for its declarative,
+          component based approach to creating and building out user interfaces.
+        </p>
+
+        <p>
+          React allows developers to break down large complex applications into
+          smaller re-usable components that are easier to manage.
+        </p>
+
+        <p>
+          A major advantage for the use of React.js is its virtual DOM, which is
+          a light weight representation of the actual DOM.
+        </p>
+
+        <p>
+          
         </p>
       </div>
     </div>
