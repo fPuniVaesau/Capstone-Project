83907667f1170816f22e1e08eefaa9a9ec2be12f
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-16 16:47:39 -0700

    working on the layout for the frontend.

--- Capstone Project/src/Components/MainContentComponents/TitleComponents/Title.jsx
+++ Capstone Project/src/Components/MainContentComponents/TitleComponents/Title.jsx
@@ -3,12 +3,14 @@ import styles from './Title.module.css';
 export default function Title({ mainTitle }) {
   return (
     <div className={styles.titleContainer}>
-      <div>
-        <p>What is</p>
-        <p>the</p>
-      </div>
-      <div>
-        <h1 className={styles.mainTitle}>{mainTitle}</h1>
+      <div className={styles.titleWrapper}>
+        <div className={styles.supportTextContainer}>
+          <p>What is</p>
+          <p>the</p>
+        </div>
+        <div>
+          <h1 className={styles.mainTitle}>{mainTitle}</h1>
+        </div>
       </div>
     </div>
   );
