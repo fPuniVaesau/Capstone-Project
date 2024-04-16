3b2fcf0064e606b3160c1e37fc58969d9a27d274
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-16 14:09:53 -0700

    working on the layout for the frontend.

--- Capstone Project/src/Components/MainContentComponents/BannerComponents/Banner.jsx
+++ Capstone Project/src/Components/MainContentComponents/BannerComponents/Banner.jsx
@@ -2,7 +2,7 @@ import styles from "./Banner.module.css"
 export default function Banner({bannerImage}){
 
   return(
-    <div className={.bannerContainer}>
+    <div className={styles.bannerContainer}>
       <img src={bannerImage} alt="" />
     </div>
   )
