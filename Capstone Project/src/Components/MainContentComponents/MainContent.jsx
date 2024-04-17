d77b8116776e16a879a92fe328a378e8ae6ea659
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-04-16 17:10:08 -0700

    working on the layout for the frontend.

--- Capstone Project/src/Components/MainContentComponents/MainContent.jsx
+++ Capstone Project/src/Components/MainContentComponents/MainContent.jsx
@@ -37,7 +37,7 @@ export default function MainContent() {
       <div className={styles.BackendArchitectureContainer}>
         <BackendArchitecture />
       </div>
-      
+
       <div className={styles.bannerContainer}>
           <Banner bannerImage={"https://images.unsplash.com/photo-1701430946144-09675a15942a?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
       </div>
