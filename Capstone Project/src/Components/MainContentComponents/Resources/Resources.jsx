b80351fcfa0df9f8ffae59392568f89070eb3eb1
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-05-21 13:52:15 -0700

    working on the layout for the frontend.

--- Capstone Project/src/Components/MainContentComponents/Resources/Resources.jsx
+++ Capstone Project/src/Components/MainContentComponents/Resources/Resources.jsx
@@ -1,10 +1,22 @@
-import styles from './Resources.module.css'
+import styles from './Resources.module.css';
 
-export default function Resources(){
-  return(
-    <div>
-      <h1>This is a test</h1>
-      <p>resources page</p>
+export default function Resources() {
+  return (
+    <div className={styles.resourceContainer}>
+      <div>
+        <h1>This is a test</h1>
+        <p>resources page</p>
+      </div>
+
+      <div>
+        <ul>
+          <li><a href="https://developer.mozilla.org/en-US/">MDN Mozilla Docs</a></li>
+          <li><a href="https://medium.com/">Medium</a></li>
+          <li><a href="https://www.youtube.com/">YouTube</a></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </div>
-  )
-};
+  );
+}
