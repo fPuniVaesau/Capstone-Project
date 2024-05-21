e4efea070e347f43aa7052c1510a9af743b652f3
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-05-21 16:54:16 -0700

    working on the layout for the frontend.

--- Capstone Project/src/Components/MainContentComponents/Resources/Resources.jsx
+++ Capstone Project/src/Components/MainContentComponents/Resources/Resources.jsx
@@ -8,13 +8,21 @@ export default function Resources() {
         <p>used to learn the Backend.</p>
       </div>
 
-      <div>
+      <div className={styles.resourceList}>
         <ul>
+          <h4 className={styles.subTitle}>Sites</h4>
           <li><a href="https://developer.mozilla.org/en-US/">MDN Mozilla Docs</a></li>
           <li><a href="https://medium.com/">Medium</a></li>
           <li><a href="https://www.youtube.com/">YouTube</a></li>
           <li><a href="https://www.codecademy.com/">Codecademy</a></li>
           <li></li>
+        </ul>
+      </div>
+
+      <div>
+        <h4 className={styles.subTitle}>Articles</h4>
+        <ul>
+
         </ul>
       </div>
     </div>
