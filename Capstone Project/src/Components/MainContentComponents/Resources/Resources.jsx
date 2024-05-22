8d89d8b77770a947dd400ae88615bd25c5455a91
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-05-21 17:12:12 -0700

    working on the layout for the frontend.

--- Capstone Project/src/Components/MainContentComponents/Resources/Resources.jsx
+++ Capstone Project/src/Components/MainContentComponents/Resources/Resources.jsx
@@ -15,18 +15,20 @@ export default function Resources() {
           <li><a href="https://medium.com/">Medium</a></li>
           <li><a href="https://www.youtube.com/">YouTube</a></li>
           <li><a href="https://www.codecademy.com/">Codecademy</a></li>
-          <li></li>
+          <li><a href="https://www.mongodb.com/docs/manual/?_ga=2.54354472.1626388945.1716336451-150035614.1713312228">MongoDB</a></li>
+          <li><a href="https://nodejs.org/en">Node.js</a></li>
+          <li><a href="https://react.dev/">React.js</a></li>
         </ul>
       </div>
 
       <div className={styles.articleContainer}>
         <h4 className={styles.subTitle}>Articles</h4>
         <ul>
+            <li><a href="https://medium.com/@namtheartist95/what-is-backend-development-bcc6a15f8472">What Is Backend Development?</a></li>
             <li><a href="https://medium.com/@asiandigitalhub/what-is-node-js-and-how-it-work-490f5ecba665">What is Node.js and How it Work?</a></li>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
+            <li><a href="https://medium.com/@nonamedev/building-full-stack-applications-with-mern-stack-a-comprehensive-guide-402e99d2e959">Building Full-Stack Applications...</a></li>
+            <li><a href="https://medium.com/nerd-for-tech/all-basics-of-mongodb-in-10-minutes-baddaf6b6625">All Basics of MongoDB in 10 Minutes</a></li>
+            <li><a href="https://medium.com/@iamme24cl/back-end-architecture-bde9e0dd06"></a>Back-end Architecture?</li>
         </ul>
       </div>
     </div>
